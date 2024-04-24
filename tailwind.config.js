864fd16b5c4663404177b83fd775103f6ac71fc8
nouraizasad <nouraizasad@hotmail.com>
2024-04-25 00:40:29 +0500

    added tailwind and custom styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {},
+    extend: {},
+  },
+  plugins: [],
+}
