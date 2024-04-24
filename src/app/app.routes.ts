e8979f74a484d1eaf6b5cd8294a9de476f551a9d
nouraizasad <nouraizasad@hotmail.com>
2024-04-25 00:40:50 +0500

    search page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './modules/events/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'events', pathMatch: 'full' },
+    { path: 'events', component: SearchComponent },
+];
